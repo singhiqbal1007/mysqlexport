883e5c951de7bc9a5e097847241cb9acd25950b1
Iqbal Singh <singhiqbal1007@gmail.com>
2021-02-01 17:31:53 +0530

    created gem

---
+++ lib/mysqlexport.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mysqlexport/version"
+
+module Mysqlexport
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mysqlexport_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Mysqlexport do
+  it "has a version number" do
+    expect(Mysqlexport::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
