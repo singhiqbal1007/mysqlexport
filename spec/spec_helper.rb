d86a44065599e34a58886b1555551644cb18d8a9
Iqbal Singh <singhiqbal1007@gmail.com>
2021-02-13 23:13:15 +0530

    fixed workflow

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,10 +8,9 @@ DB_INFO = YAML.load_file(File.join(__dir__, "config/configuration.yml"))
 CLIENT = Mysql2::Client.new({ host: DB_INFO["host"],
                               username: DB_INFO["username"],
                               password: DB_INFO["password"] })
-
-# CLIENT.query("DROP DATABASE IF EXISTS #{DB_INFO["database"]}")
-# CLIENT.query("CREATE DATABASE #{DB_INFO["database"]}")
-# CLIENT.close
+CLIENT.query("DROP DATABASE IF EXISTS #{DB_INFO["database"]}")
+CLIENT.query("CREATE DATABASE #{DB_INFO["database"]}")
+CLIENT.close
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
