3065023194fb79c3b3ca7f9026b3e59214c01d17
Iqbal Singh <singhiqbal1007@gmail.com>
2021-02-17 21:36:58 +0530

    updated run_benchmark.rb

--- spec/benchmarks/run_benchmark.rb
+++ spec/benchmarks/run_benchmark.rb
@@ -3,15 +3,19 @@ require "mysqlexport"
 require "yaml"
 require_relative "load_data"
 
-DB_INFO = YAML.load_file(File.join(__dir__, "../config/configuration.yml"))
-Benchmark::LoadData.new(DB_INFO).start unless ENV["SKIP_DATA_LOAD"].to_s.downcase == "true"
+db_info = YAML.load_file(File.join(__dir__, "../config/configuration.yml"))
+csv_path = "#{__dir__}/results/csv".freeze
+# Benchmark::LoadData.new(db_info).start unless ENV["SKIP_DATA_LOAD"].to_s.downcase == "true"
 
-Benchmark.bm(7) do |x|
-  x.report("3 lakh records:") do
-    Mysqlexport::Csv.new( host: DB_INFO["host"],
-                          username: DB_INFO["username"],
-                          password: DB_INFO["password"],
-                          database: DB_INFO["database"],
-                          execute: "select * from employees").to_path("/home/iqbal/Desktop/table.csv")
+puts "Running Benchmarks..."
+Benchmark.bm(20) do |x|
+  %w[1000 10000 100000 300000].each do |row_count|
+    options = { host: db_info["host"],
+                username: db_info["username"],
+                password: db_info["password"],
+                database: db_info["database"],
+                execute: "SELECT * FROM employees LIMIT #{row_count}",
+                output_path: "#{csv_path}/employees_#{row_count}.csv" }
+    x.report("#{"%-7d".to_s % row_count} rows: ") { Mysqlexport::Csv.new(options).to_path }
   end
 end
