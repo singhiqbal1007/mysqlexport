55ec40021740f237242272ba94d479b8ae914619
Iqbal Singh <singhiqbal1007@gmail.com>
2021-02-27 21:22:21 +0530

    Version 0.1.0

--- lib/mysqlexport/writer.rb
+++ lib/mysqlexport/writer.rb
@@ -5,7 +5,7 @@ module Writer
   end
 
   def filter_path(path)
-    path = ::Dir.pwd.to_s unless path.instance_of? String
+    path = ::Dir.pwd.to_s unless path.instance_of? ::String
     file_name = "/#{::Time.now.to_i}_mysqlexport.csv" if ::File.directory?(path)
     file_name = "/#{config.table}.csv" if ::File.directory?(path) && !config.table.nil?
     path += file_name.to_s
